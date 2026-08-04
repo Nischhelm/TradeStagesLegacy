d9e7e11eef8c7f39a2360953095d7431fcf2fbdf
nischhelm <nischhelm@gmail.com>
2026-08-04 15:27:18 +0200

    v1.1.0
    - added easiervillagertrading compat
    - fixed a crash on remote servers

--- src/main/java/tradestages/TradeStagesLegacy.java
+++ src/main/java/tradestages/TradeStagesLegacy.java
@@ -28,7 +28,7 @@ import java.util.List;
 public class TradeStagesLegacy {
    public static final String MODID = "tradestages";
    public static final String NAME = "Trade Stages Legacy";
-   public static final String VERSION = "1.0.0";
+   public static final String VERSION = "1.1.0";
    public static final Logger LOGGER = LogManager.getLogger(NAME);
 
    @Mod.EventHandler
